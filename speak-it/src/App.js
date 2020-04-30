22b0e4be98cb3ac76feafa92d7a0cc85b5a3743c
Vladimir Shevtsov <shevvv@tutanota.com>
2020-05-01 00:44:53 +0300

    feat: add App main class

---
+++ speak-it/src/App.js
@@ -0,0 +1,21 @@
+import Layout from './Elements/App/layout';
+import WordGenerator from './Elements/App/wordGenerator';
+import cardsInformation, {
+  createModal, createStartScreen, recognitionFunc, createLink,
+} from './Elements/App/helpers';
+
+/**
+ * Центральна точка приложения
+ * Конструктора нет - потому что инициализация проходит асинхронно и вызывать её надо явно и результат обрабатывать явно
+ */
+export default class App {
+
+
+  async init() {
+    this.layout = new Layout();
+    this.recognition = recognitionFunc();
+    this.wordsGenerator = new WordGenerator();
+  }
+
+
+}
